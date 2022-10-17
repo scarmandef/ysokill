d1a1417aa28decff79689a69a7a67368ad751bd6
scarmandef <80011252+scarmandef@users.noreply.github.com>
2022-10-17 09:56:06 -0300

    Update exibe.py

--- interface/banner/exibe.py
+++ interface/banner/exibe.py
@@ -1,8 +1,7 @@
 from rich import print
 
 def banner() -> str:
-  path: str = "interface/banner/banner.txt"
+    path: str = "interface/banner/banner.txt"
     with open(path) as file:
         content = file.read()
         print(f"[bold white]{content}[/]")
-
